e806c6ab2f9d3bba81db1a680a5178aaf570ca66
magicdu <johndo@magicdu.cn>
2018-05-31 23:09:53 +0800

    update html

--- src/main/java/cn/magicdu/blog/job/SchedulerTask.java
+++ src/main/java/cn/magicdu/blog/job/SchedulerTask.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class SchedulerTask {
     private int count = 0;
 
-    @Scheduled(cron = "*/6 * * * * ?")
+    //@Scheduled(cron = "*/6 * * * * ?")
     public void process() {
         System.out.println("task running"+(count++));
 
