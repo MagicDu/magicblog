ff523236e15987e2c03d6a03f862e92e9adf50df
magicdu <johndo@magicdu.cn>
2018-07-04 22:22:55 +0800

    fix

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,16 +1,27 @@
 $('#save').click(function(){
-    $.ajax({
-        type:'POST',
-        url:'/articles/savearticle',
-        data:{
-            "title":$('#title').val(),
-            "content":testEditor.getMarkdown()
-    },
-        success : function(data) {
-           mdui.alert(data.value)
-        },
-        error : function() {
-            mdui.alert("server error")
-        }
-    })
+    var content= testEditor.getMarkdown();
+    var title=$("#title").val();
+    if(title==null||title.trim()==""){
+        mdui.alert("请输入文章标题")
+    }else if(content==null||content.trim()=="")
+    {
+        mdui.alert("请输入文章内容")
+    }else{
+        $.ajax({
+            type:'POST',
+            url:'/articles/savearticle',
+            data:{
+                "articletitle":$('#title').val(),
+                "articlecontent":testEditor.getMarkdown(),
+
+            },
+            success : function(data) {
+                mdui.alert(data.value)
+            },
+            error : function() {
+                mdui.alert("server error")
+            }
+        })
+    }
+
 });
